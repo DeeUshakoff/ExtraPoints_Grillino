d90a7f94dd3867f45a6f3bfd8e71531c708a6963
DeeUshakoff <dee.ushakoff@gmail.com>
2023-09-04 11:39:02 +0300

    Добавьте файлы проекта.

---
+++ Pages/FoodDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExtraPoints_Grillino.Pages
+{
+    public class FoodDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
