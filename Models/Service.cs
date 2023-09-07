8b148a9e0b0680f2c786b7740c39a9ac6eea1464
DeeUshakoff <dee.ushakoff@gmail.com>
2023-09-07 19:57:52 +0300

    Update services

--- Models/Service.cs
+++ Models/Service.cs
@@ -4,7 +4,6 @@ namespace ExtraPoints_Grillino.Models
 {
     public class Service
     {
-
         [Key] public required string Name { get; set; }
         public required string Description { get; set; }
         public string? ImagePath { get; set; }
